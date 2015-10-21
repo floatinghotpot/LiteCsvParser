b5e105b3e28591983ec14eac1e8728b2a38b1924
floatinghotpot <rjfun.mobile@gmail.com>
2015-10-21 17:29:42 +0800

    add test data exported from win32 excel

--- Main.cs
+++ Main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 
 namespace LiteCsvParser
@@ -7,7 +9,7 @@ namespace LiteCsvParser
 	{
 		public static void TestWrite()
 		{
-			using (var writer = new CsvFileWriter("Test.csv"))
+			using (var writer = new CsvFileWriter("test.csv"))
 			{
 				// Write each row of data
 				for (int row = 0; row < 10; row++)
@@ -26,7 +28,7 @@ namespace LiteCsvParser
 		public static void TestRead()
 		{
 			List<string> fields = new List<string>();
-			using (var reader = new CsvFileReader("Test.csv"))
+			using (var reader = new CsvFileReader("test.csv"))
 			{
 				while (reader.ReadRow(fields))
 				{
@@ -43,9 +45,43 @@ namespace LiteCsvParser
 		{
 			Console.WriteLine ("Hello World!");
 
-			TestWrite ();
+			if (args.Length < 2) {
+				Console.WriteLine( "Sytnax: LiteCsvParser <input_file> <output_file>" );
+				return;
+			}
 
-			TestRead ();
+			List<List<string>> dataGrid = new List<List<string>> ();
+
+			using( var sr = new StreamReader (args [0], Encoding.GetEncoding ("gbk"))) {
+				var reader = new CsvFileReader(sr);
+				List<string> row = new List<string>();
+				while (reader.ReadRow(row)) {
+					foreach(string field in row) {
+						Console.Write("\t" + field);
+					}
+					Console.Write("\n");
+
+					dataGrid.Add( new List<string>( row ) );
+				}
+			}
+
+			Console.WriteLine ("----------------------------------------------------");
+
+			using( var sw = new StreamWriter (args [1], false, Encoding.Default) ) {
+				var writer = new CsvFileWriter(sw);
+				// Write each row of data
+				foreach(List<string> row in dataGrid) {
+
+					foreach(string field in row) {
+						Console.Write("\t" + field);
+					}
+					Console.Write("\n");
+
+					writer.WriteRow( row );
+				}
+			}
+
+			Console.WriteLine ("Done.");
 		}
 	}
 }
