196809f5bccc565934436cd9ef61e8e4cfceacb1
floatinghotpot <rjfun.mobile@gmail.com>
2015-10-22 11:47:16 +0800

    update

--- Main.cs
+++ Main.cs
@@ -7,78 +7,95 @@ namespace LiteCsvParser
 {
 	class MainClass
 	{
+		public static void PrintRow(List<string> row) {
+			foreach(string cell in row) {
+				Console.Write("\t\t" + cell);
+			}
+			Console.Write("\n");
+		}
+
 		public static void PrintDataGrid(List<List<string>> dataGrid) {
 			Console.WriteLine ("------------------------------------------------------------------------------");
 			foreach (var row in dataGrid) {
-				foreach(string cell in row) {
-					Console.Write("\t\t" + cell);
-				}
-				Console.Write("\n");
+				PrintRow(row);
 			}
 			Console.WriteLine ("------------------------------------------------------------------------------");
 		}
 
-		public static bool CompareFile(string file1, string file2) {
-			string str1 = File.ReadAllText (file1);
-			string str2 = File.ReadAllText (file2);
+		public static bool CompareFile(string inputFile, string outputFile) {
+			string str1 = File.ReadAllText (inputFile);
+			string str2 = File.ReadAllText (outputFile);
 			bool same = (str1 == str2);
-			Console.WriteLine ("inputFile" + (same?" == ":" != ") + "outputFile");
+
+			Console.WriteLine ("input file: " + inputFile);
+			Console.WriteLine ("output file: " + outputFile);
+			Console.WriteLine ("TEST " + (same?"PASS":"FAIL"));
 			return same;
 		}
 
-		public static bool Test1(string inputFile, string outputFile) {
+		public static bool TestWithGrid(string inputFile, string outputFile) {
 			var dataGrid = CsvFileReader.ReadAll(inputFile, Encoding.GetEncoding("gbk"));
-			if (dataGrid == null) {
+			if (dataGrid != null) {
+				PrintDataGrid (dataGrid);
+
+				CsvFileWriter.WriteAll(dataGrid, outputFile, Encoding.GetEncoding("gbk"));
+
+				return CompareFile (inputFile, outputFile);
+
+			} else {
 				Console.WriteLine("Failed loading data from inputFile");
 				return false;
 			}
-
-			PrintDataGrid (dataGrid);
-
-			CsvFileWriter.WriteAll(dataGrid, outputFile, Encoding.GetEncoding("gbk"));
-
-			return CompareFile (inputFile, outputFile);
 		}
 
-		public static bool Test2(string inputFile, string outputFile) {
-			var dataGrid = new List<List<string>> ();
-
+		public static bool TestWithRow(string inputFile, string outputFile) {
 			using (var sr = new StreamReader (inputFile, Encoding.GetEncoding ("gbk"))) {
 				var reader = new CsvFileReader (sr);
-				// read row by row
-				var row = new List<string> ();
-				while (reader.ReadRow(row)) {
-					dataGrid.Add (new List<string> (row));
-				}
-			}
+				using( var sw = new StreamWriter (outputFile, false, Encoding.GetEncoding ("gbk")) ) {
+					var writer = new CsvFileWriter(sw);
 
-			PrintDataGrid (dataGrid);
-			
-			using( var sw = new StreamWriter (outputFile, false, Encoding.GetEncoding ("gbk")) ) {
-				var writer = new CsvFileWriter(sw);
-				// Write each row of data
-				foreach(var row in dataGrid) {
-					writer.WriteRow( row );
+					Console.WriteLine ("------------------------------------------------------------------------------");
+					var row = new List<string> ();
+					while (reader.ReadRow(row)) {
+						PrintRow(row);
+						writer.WriteRow( row );
+					}
+					Console.WriteLine ("------------------------------------------------------------------------------");
 				}
 			}
 
 			return CompareFile (inputFile, outputFile);
 		}
 
-		public static void Main (string[] args)
-		{
+		public static void Main (string[] args) {
 			Console.WriteLine ("Hello World!");
 
-			if (args.Length < 2) {
-				Console.WriteLine( "Sytnax: LiteCsvParser <input_file> <output_file>" );
+			if (args.Length < 1) {
+				Console.WriteLine( "Sytnax: \nLiteCsvParser <input_file> <input_file> ...\nLiteCsvParser *.csv" );
 				return;
 			}
 
-			string inputFile = args [0], outputFile = args [1];
+			int nPass = 0;
+			List<string> failed = new List<string> ();
+			for (int i=0; i<args.Length; i++) {
+				bool ok = TestWithGrid (args[i], "./tmp.csv");
+				if(ok) {
+					nPass ++;
+				} else {
+					failed.Add(args[i]);
+				}
+			}
+			File.Delete ("./tmp.csv");
 
-			Test1 (inputFile, outputFile);
+			Console.WriteLine("\nTest pass: " + nPass.ToString() + "/" + args.Length.ToString());
 
-			Test2 (inputFile, outputFile);
+			if (failed.Count > 0) {
+				Console.WriteLine("Failed cases:");
+				foreach(var filepath in failed) {
+					Console.WriteLine( filepath );
+				}
+			}
+			Console.WriteLine ();
 		}
 	}
 }
